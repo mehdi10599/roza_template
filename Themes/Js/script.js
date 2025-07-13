1263cf832e7a66988fd588b0ff1c154b27bc52b0
Mehdi10599 <nimaa325@gmail.com>
2025-07-13 17:51:07 +0330

    responsive until 960px

--- Themes/Js/script.js
+++ Themes/Js/script.js
@@ -22,4 +22,33 @@
         $(".zr-basket-buttons > a:nth-child(2)").on("click",function(){
             $(".zr-header-basket-main").toggle();
         });
+
+
+
+
+
+        //hamber menu
+        $(".zr-hamber-menu-icon,.zr-hamber-menu-close").on("click",function(e){
+            $(".zr-hamer-container-fixed ,.zr-hamber-menu-icon,.zr-hamber-menu-close").toggleClass("zr-collapse");
+        });
+        
+        //mahsoolat
+        $(".zr-hamber-container>a:nth-child(2)").on("click",function(e){
+            $(".zr-mahsoolat-arrow").toggleClass("zr-rotate");
+            $(".zr-hamber-submenu ").toggle();
+        });
+
+        //submenu
+        $(".zr-hamber-submenu-items").on("click",function(event){
+            event.stopPropagation();
+            $(this).find("ul.zr-hamber-submenu-item").toggleClass("zr-collapse");
+            $(this).find(".zr-sub-arrow").toggleClass("zr-rotate");
+        });
+        
+        $(".zr-hamber-submenu-item ").on("click",function(event){
+            event.stopPropagation();
+        });
+        
+
+
     });
